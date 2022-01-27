d822026f614273c0176eaed0169b110d69953f90
Ninny <MKLNinny@qq.com>
2022-01-27 20:40:56 +0800

    Alter text.

--- WinmineCheater/Address.cs
+++ WinmineCheater/Address.cs
@@ -15,7 +15,7 @@
         /// </summary>
         internal static readonly int MINE_START_ADDRESS = 0x01005361;
         /// <summary>
-        /// Is enable "?" signature. 1: Enable, 2: Disable.
+        /// Is enable "?" signature. 1: Enable, 0: Disable.
         /// </summary>
         internal static readonly int IS_ENABLE_QUESTION_MARK = 0x010056BC;
     }
